0364f14fd37911991322359b8342dd98e3d1fa3e
Ayush Dwivedi <amayush0818@gmail.com>
2023-06-17 14:23:23 +0530

    Navbar Done

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ import {
   loadExchange
 } from '../store/interactions';
 
+import Navbar from './Navbar';
+
 function App() {
 
   const dispatch = useDispatch();
@@ -23,9 +25,16 @@ function App() {
 
     // Fetch current network's chainId (e.g, hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
+
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
     
-    // Fetch current account and balance from Metamask
-    await loadAccount(provider, dispatch)
+    // Fetch current account and balance from Metamask when changed
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch) 
+    })
 
     // load token Smart Contract
     const DeCo = config[chainId].DeCo
@@ -45,7 +54,7 @@ function App() {
   return (
     <div>
 
-        {/* Navbar */}
+        <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
