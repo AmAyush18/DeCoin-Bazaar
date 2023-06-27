823362a417ceb75ec0a4d1023087c27018a91bbd
Ayush Dwivedi <amayush0818@gmail.com>
2023-06-27 19:42:07 +0530

    Fill Order implemented

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -1,12 +1,20 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import sort from '../assets/sort.svg';
 
 import { orderBookSelector } from "../store/selectors";
+import { fillOrder } from "../store/interactions";
 
 const OrderBook = () => {
     const symbols = useSelector(state => state.tokens.symbols)
-
+    const provider = useSelector(state => state.provider.connection)
+    const exchange = useSelector(state => state.exchange.contract)
     const orderBook = useSelector(orderBookSelector)
+
+    const dispatch = useDispatch()
+
+    const fillOrderHandler = (order) => {
+      fillOrder(provider, exchange, order, dispatch)
+    }
 
     return (
       <div className="component exchange__orderbook">
@@ -34,7 +42,7 @@ const OrderBook = () => {
 
                 {orderBook && orderBook.sellOrders.map((order, index) => {
                   return (
-                    <tr key={index}>
+                    <tr key={index} onClick={() => fillOrderHandler(order)}>
                       <td>{order.token0Amount}</td>
                       <td style={{ color: `${order.orderTypeClass}` }}>{order.tokenPrice}</td>
                       <td>{order.token1Amount}</td>
@@ -67,7 +75,7 @@ const OrderBook = () => {
               
               {orderBook && orderBook.buyOrders.map((order, index) => {
                     return (
-                      <tr key={index}>
+                      <tr key={index} onClick={() => fillOrderHandler(order)}>
                         <td>{order.token0Amount}</td>
                         <td style={{ color: `${order.orderTypeClass}` }}>{order.tokenPrice}</td>
                         <td>{order.token1Amount}</td>
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -83,6 +83,9 @@ const DEFAULT_EXCHANGE_STATE = {
     cancelledOrders : {
         data: []
     },
+    filledOrders : {
+        data: []
+    },
     events: []
   }
 
@@ -99,6 +102,34 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
 
         //--------ORDERS LOADED (CANCELLED, FILLED & ALL) ---------
 
+        case 'CANCELLED_ORDERS_LOADED':
+            return {
+            ...state,
+            cancelledOrders: {
+                loaded: true,
+                data: action.cancelledOrders
+            }
+            }
+    
+        case 'FILLED_ORDERS_LOADED':
+            return {
+            ...state,
+            filledOrders: {
+                loaded: true,
+                data: action.filledOrders
+            }
+            }
+    
+        case 'ALL_ORDERS_LOADED':
+            return {
+            ...state,
+            allOrders: {
+                loaded: true,
+                data: action.allOrders
+            }
+            }
+  
+        // ------ CANCELLING ORDER CASE-----
         case 'ORDER_CANCEL_REQUEST':
             return {
                 ...state,
@@ -138,6 +169,51 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
                 }
             }
 
+        //---------- FILLING ORDERS ----------------
+        case 'ORDER_FILL_REQUEST':
+            return {
+                ...state,
+                transaction: {
+                    transactionType: "Fill Order",
+                    isPending: true,
+                    isSuccessful: false
+                }
+            }
+
+        case 'ORDER_FILL_SUCCESS':
+            // Prevent duplicate orders
+            index = state.filledOrders.data.findIndex(order => order.id.toString() === action.order.id.toString())
+
+            if (index === -1) {
+                data = [...state.filledOrders.data, action.order]
+            } else {
+                data = state.filledOrders.data
+            }
+
+            return {
+                ...state,
+                transaction: {
+                    transactionType: "Fill Order",
+                    isPending: false,
+                    isSuccessful: true
+                },
+                filledOrders: {
+                    ...state.filledOrders,
+                    data
+                },
+                events: [action.event, ...state.events]
+            }
+
+        case 'ORDER_FILL_FAIL':
+            return {
+                ...state,
+                transaction: {
+                    transactionType: "Fill Order",
+                    isPending: false,
+                    isSuccessful: false,
+                    isError: true
+                }
+            }
         //---------- BALANCE CASES ------------------
         case 'EXCHANGE_TOKEN_1_BALANCE_LOADED':
             return {
